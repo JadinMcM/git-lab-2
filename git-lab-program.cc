15588f39408785190962fcb684273b4ea1de2226
Jadin McManus <jm431318@ohio.edu>
2020-01-21 10:48:54 -0500

    Hello Git!

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jadin McManus
+ *        Date: 21 January 2020
+ * Description: First github lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+ 
